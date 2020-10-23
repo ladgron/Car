e1bb22080c3bd28b18ff41ee112e4b2cab9be5d5
Ladan G <ladgron@gmail.com>
2020-10-23 11:23:45 +0200

    initial commit

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+/**
+ * Created by LG
+ * Date: 2020-10-23
+ * Time: 10:33
+ * Project: myTry
+ * Copyright: MIT
+ * To change this template use File | Settings | File Templates.
+ */
+public class Car {
+ int modelyear;
+ String modelName;
+ int price;
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-   System.out.println("hi!");
+
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        car1.modelyear = 1980;
+        car1.modelName = "Volvo";
+        car1.price = 15000;
+
+
+        car2.modelyear = 2000;
+        car2.modelName = "SAAB";
+        car2.price = 20000;
+
+        System.out.printf("%s %d kostar %,d kr \n", car1.modelName, car1.modelyear, car1.price);
+        System.out.printf("%s %d kostar %,d kr \n", car2.modelName, car2.modelyear, car2.price);
     }
 }
