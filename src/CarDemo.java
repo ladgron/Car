4ea2cc43ca9acf772e335cd3e22109ba47ea52c7
Ladan G <ladgron@gmail.com>
2020-10-23 12:28:49 +0200

    initial commit

--- src/CarDemo.java
+++ src/CarDemo.java
@@ -1,4 +1,4 @@
-public class Main {
+public class CarDemo {
 
     public static void main(String[] args) {
 	// write your code here
---
+++ src/Point.java
@@ -0,0 +1,19 @@
+/**
+ * Created by LG
+ * Date: 2020-10-23
+ * Time: 11:51
+ * Project: myTry
+ * Copyright: MIT
+ * To change this template use File | Settings | File Templates.
+ */
+
+// Point är en klass som beskriver punkter.
+//i ett vanligt tvådimensionellt koordinatsystem. (en punkt i 2D koordinats)
+
+
+public class Point {
+
+    // Instansvariabler
+    double x;  // x-koordinat
+    double y;  // y-koordinat
+}
